b2b515e3a540cab5e2caa8099907b0633e4efb91
Keiden Byrne <keiden.byrne@stu.ocsb.ca>
2023-02-22 19:01:16 +0000

    Constants fix

--- Microwave.java
+++ Microwave.java
@@ -26,7 +26,7 @@ public final class Microwave {
     * @param args a placeholder value when making the main function
     */
     public static void main(String[] args) {
-        final Scanner get = new Scanner(System.in);
+        final Scanner GET_IN = new Scanner(System.in);
         double time = 0;
         double modifier = 0.0;
         // correctFood used later to make sure nothing is outputted
@@ -38,30 +38,30 @@ public final class Microwave {
             + "What food are you microwaving? (Pizza, Sub, Soup)");
         // Put the .toUpperCase() modifier here so it doesn't need to
         // be retyped for every if statement later.
-        final String foodItem = get.nextLine().toUpperCase();
+        final String FOOD_ITEM = GET_IN.nextLine().toUpperCase();
         System.out.println("And how much of that are you microwaving?");
-        final String quantityStr = get.nextLine();
+        final String QUAN_STR = GET_IN.nextLine();
 
         try {
-            final double quantityDouble = Double.parseDouble(quantityStr);
+            final double QUAN_DOU = Double.parseDouble(QUAN_STR);
 
             // Makes sure the program only continues if the quantity of
             // food put in the microwave is between 4 and 0.
-            if (quantityDouble > 4) {
+            if (QUAN_DOU > 4) {
                 // Message that is sent if more than 4 pieces are
                 // put into the microwave
                 System.out.printf("The microwave is too small for %.0f"
-                    + " pieces!\n", quantityDouble);
-            } else if (quantityDouble > 0) {
+                    + " pieces!\n", QUAN_DOU);
+            } else if (QUAN_DOU > 0) {
                 // Decides how much the time needs to be increased or
                 // Decreased depending on the amount.
-                modifier = (quantityDouble - 1) / 2 + 1;
+                modifier = (QUAN_DOU - 1) / 2 + 1;
 
-                if ("PIZZA".equals(foodItem)) {
+                if ("PIZZA".equals(FOOD_ITEM)) {
                     time = 45;
-                } else if ("SUB".equals(foodItem)) {
+                } else if ("SUB".equals(FOOD_ITEM)) {
                     time = 60;
-                } else if ("SOUP".equals(foodItem)) {
+                } else if ("SOUP".equals(FOOD_ITEM)) {
                     time = 105;
                 } else {
                     System.out.println("Our scientists have not discovered"
@@ -72,20 +72,20 @@ public final class Microwave {
                 }
 
                 if (correctFood) {
-                    final double totalTime = modifier * time;
+                    final double TOTAL_TIME = modifier * time;
                     System.out.printf("You should microwave these for %.0fm,"
-                        + " %.0fs.\n", totalTime / 60, totalTime % 60);
+                        + " %.0fs.\n", TOTAL_TIME / 60, TOTAL_TIME % 60);
                 }
             } else {
                 // Message that is sent if less than 4 pieces
                 // are put into the microwave.
                 System.out.printf("You can't put %.0f pieces of"
-                    + " food in a microwave.\n", quantityDouble);
+                    + " food in a microwave.\n", QUAN_DOU);
             }
         } catch (NumberFormatException error) {
             System.out.println("That's not an amount of food. "
                 + error.getMessage());
         }
-        get.close();
+        GET_IN.close();
     }
 }
